7703beb89254082a78b796cd3f7d35db86e1db4a
TomOzkan <tomozkan0@icloud.com>
2025-01-06 02:13:36 +0100

    wip: enhance header and homepage layout

--- src/app/(WebSite)/components/Header/Header.tsx
+++ src/app/(WebSite)/components/Header/Header.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="z-10 flex flex-row items-center justify-between p-2 text-white">
+      <Image src="img/logo-light.svg" alt="Logo" width={100} height={100} />
+      <nav>
+        <ul className="flex flex-row space-x-4">
+          <li>Accueil</li>
+          <li>Services</li>
+          <li>A Propos</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <button className="bg-primary-500 p-2">Rendez-vous</button>
+    </header>
+  );
 }
--- src/app/(WebSite)/page.tsx
+++ src/app/(WebSite)/page.tsx
@@ -1,11 +1,51 @@
 import { homeMetadata } from "@/constants/metadatas";
-
+import Image from "next/image";
 export const metadata = homeMetadata;
 
 export default function HomePage() {
   return (
-    <main>
-      <h1>Febryer en cours de construction...</h1>
+    <main className="">
+      {/* Image and Banner Container */}
+      <div className="absolute left-0 top-0 h-full w-full">
+        <Image
+          src="/img/Bg-wip-cloudinary.png"
+          alt="Background Image"
+          className="h-full w-full object-cover"
+          layout="fill"
+          priority
+        />
+        {/* Banner positioned at the bottom of the image */}
+        <div className="absolute bottom-0 left-0 w-full bg-primary-500 p-4 text-white">
+          <div className="flex flex-row gap-4 divide-x">
+            <div>
+              <p>Contactez-nous maintenant</p>
+              <p className="text-sm">+33 6 30 56 90 33</p>
+            </div>
+            <div>
+              <p className="ml-4">Localisation</p>
+              <p className="ml-4 text-sm">Secteur - Roannais</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Content */}
+      <div className="relative z-10 my-0 p-4 uppercase text-white transition-all duration-700 sm:my-12 md:my-16 lg:my-20 xl:my-24">
+        <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl">
+          <span className="text-primary-500">Toujours</span> brillant
+        </h1>
+        <h2 className="mt-4 text-2xl font-bold sm:text-3xl md:text-4xl">
+          <span className="text-primary-500">Toujours</span> prêt à rouler
+        </h2>
+        <button className="mt-4 bg-primary-500 p-2 md:p-4 md:text-xl">
+          Rendez-vous
+        </button>
+      </div>
+
+      {/* Footer Text */}
+      <h1 className="relative z-10 text-white">
+        Febryer en cours de construction...
+      </h1>
     </main>
   );
 }
